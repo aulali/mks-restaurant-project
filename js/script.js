18bc1d35e9c09a48ea8bab78f036548e583c8a5d
aulali <aulali@Autumns-MacBook-Air.local>
2014-10-22 20:56:31 -0500

    finished styling except social links and javascript except last todo

--- js/script.js
+++ js/script.js
@@ -5,12 +5,7 @@ $(function() {
 
     // Note: my menu section headings look like this:
     //
-    // <div class="menu-section">
-    //   <h3>
-    //     <a href="#" class="menu-section-item" id="dinner">
-    //       Dinner
-    //     </a>
-    //   </h3>
+  
     //   ...
 
     // I've given all of the anchors the same class so that I can easily target
@@ -21,17 +16,20 @@ $(function() {
       event.preventDefault();
 
       // Assign the id of the clicked element (this) to a variable named id
-      var  = $( this );
+      var course = this.id;
+      console.log(course);
+
 
       // Remove the class 'is-active' from all menu item headings
-      $( '' ).removeClass( '' );
+      $( 'h3' ).removeClass( 'is-active' );
 
       // Add 'is-active' to this specific action that was clicked (this). is-active
       // provides the visual cue for what's active via CSS
-      $( this ).addClass( '' );
+      $( this ).addClass( 'is-active' );
 
       // Once you're started with TODO #2, call the getMenu function here,
       // passing id as the argument
+      getMenu(course);
 
     });
 
@@ -69,7 +67,7 @@ $(function() {
 
       // start a for loop that iterates through json.length
       // add json.length into this for loop code
-      for( var i = 0; i < ; i++ ){
+      for( var i = 0; i < json.length; i++ ){
         // wrap each section in a menu-group div
         html += '<div class="menu-group columns small-12 medium-4">';
         // append inside the menu-group div a h4 with the json section name in it
@@ -84,9 +82,9 @@ $(function() {
           html += '<div class="menu-item">';
           // inside each menu-item div, create a div for dish, ingredients, and price
           // add json[i]content[j].THING where THING is dish, ingredient, price.
-          html += '<div class="menu-item-dish">' + json[i].content[j]. + '</div>';
-          html += '<p class="menu-item-ingredients">' + json[i].content[j]. + '</p>';
-          html += '<div class="menu-item-price">' + json[i].content[j]. + '</div>';
+          html += '<div class="menu-item-dish">' + json[i].content[j].dish + '</div>';
+          html += '<p class="menu-item-ingredients">' + json[i].content[j].ingredient + '</p>';
+          html += '<div class="menu-item-price">' + json[i].content[j].price + '</div>';
           html += '</div>';
         }
 
